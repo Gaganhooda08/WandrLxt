cf53f407a25e90f9549adc1a11b72dd802df7c22
Gaganhooda08 <mrgaganhooda@gmail.com>
2025-09-25 12:17:44 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,33 @@
+// const e = require("connect-flash");
+
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: listing.geometry.coordinates,
+    zoom: 9,
+});
+
+const marker = new mapboxgl.Marker({color: "red"})
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    .setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after booking</p>`))
+    .addTo(map);
+
+
+
+
+
+    // mapboxgl.accessToken = mapToken;
+
+    // const map = new mapboxgl.Map({
+    //     container: 'map', // container ID
+    //     center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    //     zoom: 9 // starting zoom
+    // });
+
+
+    // const marker = new mapboxgl.Marker()
+    // .setLngLat([coordinates])
+    // .addTo(map);
